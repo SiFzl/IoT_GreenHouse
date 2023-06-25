6982e5edbe0c746b6271c4e35b91f86616eb938a
SiFzl <fazel.sinajan@yahoo.ca>
2023-06-25 14:29:19 +0330

    Arduino Code
    This is our Arduino code for the project. Including: LDR, MQ_2, DHT_22, LM335 and Soil Moisture sensors; Also include Servo Motor SG_90 and Electrical Valve.

---
+++ multiple.ino
@@ -0,0 +1,131 @@
+#include <DHT.h>;
+#include <Servo.h>
+
+
+//Constants
+#define SoilMPin A1
+#define LDRPIN = A2
+#define DHTPIN 8     
+#define DHTTYPE DHT22   
+DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
+#define MQ2pin A3
+#define Threshold 400
+#define Threshold2 200
+#define Thresholdmin 700
+#define Thresholdmax 800
+
+//servo_motors
+Servo servo1;
+Servo servo2;
+
+//Variables
+int LDRsensor = 0; //Stores light level
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+const int LMPin = A0; //Stores temperature value
+
+float LMsensor;
+float voltageOut;
+float temperatureK;
+float temperatureC;
+float temperatureF;
+
+float MQValue;
+
+//solenoid_valve
+int solenoidPin = 4;
+
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  servo1.attach(9);
+  servo2.attach(10);
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(solenoidPin, OUTPUT);
+  Serial.println("MQ2 warming up!");
+  servo1.write(0);
+  servo2.write(0);
+	delay(20000); // allow the MQ2 to warm up
+}
+
+void loop()
+{
+    
+    delay(2000);
+    
+    //{MQ_2}
+    MQValue = analogRead(MQ2pin);
+
+    //{capacitive_sensor}
+    int soil_moisture = analogRead(SoilMPin);//
+    soil_moisture = map(soil_moisture , Thresholdmin, Thresholdmax, 0, 100 );
+
+    //{LDR}
+    LDRsensor = analogRead(A2);
+    
+    //{LM335}
+    LMsensor = analogRead(A0);
+    voltageOut = (LMsensor * 5000) / 1024;
+    temperatureK = voltageOut / 10;
+    temperatureC = temperatureK - 273; 
+    temperatureF = (temperatureC * 1.8) + 32;
+
+    //{DHT22}
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    
+    //{LM335}
+    Serial.print("Temperature(ºC): "); 
+    Serial.print(temperatureC);  
+
+    //{DHT22}
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+
+    //{capacitive_sensor}
+    Serial.print("Soil Moisture: ");
+    Serial.println(soil_moisture);
+
+    //{LDR}
+    Serial.print("Light: ");
+    Serial.print(LDRsensor);
+
+    Serial.print("Smoke level: ");
+    Serial.print(MQValue);
+
+    if(MQValue > Threshold)
+    {
+     Serial.print(" | Smoke detected!");
+    }
+  
+    Serial.println("");
+
+    if(soil_moisture < 0)
+    {
+      digitalWrite(solenoidPin, LOW);
+      Serial.println("valve closed");
+    }
+
+    if(soil_moisture > 100)
+    {
+      digitalWrite(solenoidPin, HIGH);
+      Serial.println("valve open");
+    }
+
+
+    if(LDRsensor > Threshold2)
+    {
+      servo1.write(90);
+      servo2.write(90);
+      Serial.print(" | blinders closed!");
+    }
+
+    delay(10000); //Delay 2 sec.
+}
+
+   
